d9e044457af3c588fc73b7912664094bfac92876
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2025-02-21 00:19:48 -0800

    Update DisasterVictim.java
    test errors in:
    - personalBelongings
    - familyConnections

--- src/edu/ucalgary/oop/DisasterVictim.java
+++ src/edu/ucalgary/oop/DisasterVictim.java
@@ -5,7 +5,10 @@ public class DisasterVictim {
 	private String firstName;
 	private String lastName;
 	private String dateOfBirth;
-	private int ASSIGNED_SOCIAL_ID;
+	private int ASSIGNED_SOCIAL_ID = -1;
+	/*ID will never be negative, this tells the program that the victim has not generated
+	an assigned social ID yet.*/
+	
 	private FamilyRelation[] familyConnections;
 	private MedicalRecord[] medicalRecords;
 	private Supply[] personalBelongings;
@@ -60,6 +63,22 @@ public class DisasterVictim {
 		return this.ENTRY_DATE;
 	}
 
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public void setDateOfBirth(String dateOfBirth) throws IllegalArgumentException {
 		//Date format should be in YYYY-MM-DD format, throws exception otherwise
 		if (isValidDateFormat(dateOfBirth)) {
@@ -69,6 +88,63 @@ public class DisasterVictim {
 		}
 	}
 	
+	public int getAssignedSocialID() {
+		if (this.ASSIGNED_SOCIAL_ID == -1) {
+			this.ASSIGNED_SOCIAL_ID = generateSocialID();
+			return this.ASSIGNED_SOCIAL_ID;
+		} else {
+			return this.ASSIGNED_SOCIAL_ID;
+		}
+	}
+	
+	public FamilyRelation[] getFamilyConnections() {
+		return familyConnections;
+	}
+
+	public void setFamilyConnections(FamilyRelation[] familyConnections) {
+		this.familyConnections = familyConnections;
+	}
+
+	public MedicalRecord[] getMedicalRecords() {
+		return medicalRecords;
+	}
+
+	public void setMedicalRecords(MedicalRecord[] medicalRecords) {
+		this.medicalRecords = medicalRecords;
+	}
+
+	public Supply[] getPersonalBelongings() {
+		return personalBelongings;
+	}
+
+	public void setPersonalBelongings(Supply[] personalBelongings) {
+		this.personalBelongings = personalBelongings;
+	}
+	
+	public void addPersonalBelonging(Supply supply) {
+		Supply[] newPersonalBelongings = new Supply[this.personalBelongings.length + 1];
+		
+		int i = 0;
+		for (Supply belonging : this.personalBelongings) {
+			newPersonalBelongings[i] = belonging;
+			i++;
+		}
+		
+		this.personalBelongings = newPersonalBelongings;
+	}
+	
+	public void removePersonalBelonging(Supply unwantedSupply) {
+		//TODO implement method
+	}
+	
+	public void addFamilyConnection(FamilyRelation record) {
+		//TODO implement method
+	}
+	
+	public void removeFamilyConnection(FamilyRelation exRelation) {
+		//TODO implement method
+	}
+
 	private static boolean isValidDateFormat(String date) {
 		Pattern dateRegex = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}");
 		Matcher dateMatcher = dateRegex.matcher(date);
@@ -78,5 +154,10 @@ public class DisasterVictim {
 		} else {
 			return false;
 		}
+	}
+	
+	private static int generateSocialID() {
+		DisasterVictim.counter++;
+		return DisasterVictim.counter;
 	}
 }
