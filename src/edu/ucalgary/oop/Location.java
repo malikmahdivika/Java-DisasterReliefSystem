e89bff3cf792af322ff9dd95d3a36a72112c50ed
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2025-02-20 23:44:26 -0800

    MedicalRecord tests all pass
    with incomplete Location functionality to ensure MedicalRecord passes tests

--- src/edu/ucalgary/oop/Location.java
+++ src/edu/ucalgary/oop/Location.java
@@ -1,5 +1,29 @@
 package edu.ucalgary.oop;
 
 public class Location {
+	private String name;
+	private String address;
+	private DisasterVictim[] occupants;
+	private Supply[] supplies;
+	
+	public Location(String name, String address) {
+		this.setName(name);
+		this.setAddress(address);
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
--- src/edu/ucalgary/oop/MedicalRecord.java
+++ src/edu/ucalgary/oop/MedicalRecord.java
@@ -1,5 +1,55 @@
 package edu.ucalgary.oop;
 
-public class MedicalRecord {
+import java.util.regex.*;
 
+public class MedicalRecord {
+	private Location location;
+	private String treatmentDetails;
+	private String dateOfTreatment;
+	
+	public MedicalRecord(Location location, String treatmentDetails,
+			String dateOfTreatment) throws IllegalArgumentException {
+		this.setLocation(location);
+		this.setTreatmentDetails(treatmentDetails);
+		this.setDateOfTreatment(dateOfTreatment);
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getTreatmentDetails() {
+		return treatmentDetails;
+	}
+
+	public void setTreatmentDetails(String treatmentDetails) {
+		this.treatmentDetails = treatmentDetails;
+	}
+	
+	public String getDateOfTreatment() {
+		return dateOfTreatment;
+	}
+
+	public void setDateOfTreatment(String dateOfTreatment) throws IllegalArgumentException {
+		if (isValidDateFormat(dateOfTreatment)) {
+			this.dateOfTreatment = dateOfTreatment;
+		} else {
+			throw new IllegalArgumentException("Date must be in format YYYY-MM-DD");
+		}
+	}
+	
+	private static boolean isValidDateFormat(String date) {
+		Pattern dateRegex = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}");
+		Matcher dateMatcher = dateRegex.matcher(date);
+		
+		if (dateMatcher.matches()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
