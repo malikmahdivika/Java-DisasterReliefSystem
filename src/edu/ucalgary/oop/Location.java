28d58dfc6e4bc8316f23529c9019544961b1112d
jsanjuan <jessica.sanjuan@ucalgary.ca>
2025-02-27 00:45:07 -0700

    Implemented Location and ReliefService classes

--- src/edu/ucalgary/oop/Location.java
+++ src/edu/ucalgary/oop/Location.java
@@ -1,14 +1,18 @@
 package edu.ucalgary.oop;
 
+import java.util.ArrayList;
+
 public class Location {
 	private String name;
 	private String address;
-	private DisasterVictim[] occupants;
-	private Supply[] supplies;
-	
+	private ArrayList<DisasterVictim> occupants;
+	private ArrayList<Supply> supplies;
+
 	public Location(String name, String address) {
-		this.setName(name);
-		this.setAddress(address);
+		this.name = name;
+		this.address = address;
+		this.occupants = new ArrayList<>();
+		this.supplies = new ArrayList<>();
 	}
 
 	public String getName() {
@@ -25,5 +29,37 @@ public class Location {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public ArrayList<DisasterVictim> getOccupants() {
+		return new ArrayList<>(occupants);
+	}
+
+	public void setOccupants(ArrayList<DisasterVictim> occupants) {
+		this.occupants = new ArrayList<>(occupants);
+	}
+
+	public void addOccupant(DisasterVictim victim) {
+		this.occupants.add(victim);
+	}
+
+	public void removeOccupant(DisasterVictim victim) {
+		this.occupants.remove(victim);
+	}
+
+	public ArrayList<Supply> getSupplies() {
+		return new ArrayList<>(supplies);
+	}
+
+	public void setSupplies(ArrayList<Supply> supplies) {
+		this.supplies = new ArrayList<>(supplies);
+	}
+
+	public void addSupply(Supply supply) {
+		this.supplies.add(supply);
+	}
+
+	public void removeSupply(Supply supply) {
+		this.supplies.remove(supply);
 	}
 }
--- src/edu/ucalgary/oop/ReliefService.java
+++ src/edu/ucalgary/oop/ReliefService.java
@@ -1,5 +1,58 @@
 package edu.ucalgary.oop;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Pattern;
+
 public class ReliefService {
 
+    private Inquirer inquirer;
+    private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+
+    private static final String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
+
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        setDateOfInquiry(dateOfInquiry);
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
+
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    // helper function
+    public void setDateOfInquiry(String dateOfInquiry) {
+        if (!Pattern.matches(DATE_PATTERN, dateOfInquiry)) {
+            throw new IllegalArgumentException("Invalid date format. Expected format: YYYY-MM-DD");
+        }
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public String getLogDetails() {
+        return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: "
+                + missingPerson.getFirstName() + ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: "
+                + infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
+    }
 }
