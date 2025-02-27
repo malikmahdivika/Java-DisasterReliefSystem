e7723ee3ac648f92a096649ebefe3eb4622a570c
uosasekhator <osadebamwen.ekhator@ucalgary.ca>
2025-02-27 03:25:23 -0700

    Update ReliefService.java
    I added the isValidDateFormat method and the setters corresponding to their associated variables and getters

--- src/edu/ucalgary/oop/ReliefService.java
+++ src/edu/ucalgary/oop/ReliefService.java
@@ -12,7 +12,7 @@ public class ReliefService {
     private String infoProvided;
     private Location lastKnownLocation;
 
-    private static final String DATE_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
+    private static final String DATE_PATTERN = "^\\d{4}-\\d{2}-\\d{2}$";
 
     public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
         this.inquirer = inquirer;
@@ -26,21 +26,32 @@ public class ReliefService {
         return inquirer;
     }
 
+    public void setInquirer(Inquirer inquirer) {
+		this.inquirer = inquirer;
+	}
+
     public DisasterVictim getMissingPerson() {
         return missingPerson;
     }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+		this.missingPerson = missingPerson;
+	}
 
     public String getDateOfInquiry() {
         return dateOfInquiry;
     }
 
     // helper function
-    public void setDateOfInquiry(String dateOfInquiry) {
-        if (!Pattern.matches(DATE_PATTERN, dateOfInquiry)) {
-            throw new IllegalArgumentException("Invalid date format. Expected format: YYYY-MM-DD");
-        }
-        this.dateOfInquiry = dateOfInquiry;
-    }
+    public void setDateOfInquiry(String dateOfInquiry) throws IllegalArgumentException {
+		boolean checkDate = isValidDateFormat(dateOfInquiry);
+		if(checkDate == false) {
+			throw new IllegalArgumentException("Invalid date format!");
+		}
+		else {
+			this.dateOfInquiry = dateOfInquiry;
+		}
+	}
 
     public String getInfoProvided() {
         return infoProvided;
@@ -49,6 +60,26 @@ public class ReliefService {
     public Location getLastKnownLocation() {
         return lastKnownLocation;
     }
+
+    private boolean isValidDateFormat(String date) {
+		Pattern dateFormat = Pattern.compile(DATE_PATTERN);
+		Matcher myMatcher = dateFormat.matcher(date);
+		
+		if(myMatcher.find()) {
+			String month = date.substring(5,7);
+			String day = date.substring(8, 10);
+			if(Integer.parseInt(month) > 12 || Integer.parseInt(month) < 0) {
+				return false;
+			}
+			else if(Integer.parseInt(day) > 31 || Integer.parseInt(day) < 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+		return true;
+	}
 
     public String getLogDetails() {
         return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: "
